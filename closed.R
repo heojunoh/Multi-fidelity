5250080912f525d023d62faea9783b89dbe57a69
Junoh <junoh@heojun-oui-MacBook-Pro.local>
2023-04-01 17:09:24 -0400

    Scaling before fitting and then scaling back after fitting. This makes the result stable.

--- closed.R
+++ closed.R
@@ -1,19 +1,21 @@
 ### Function to calculate the closed form ###
 closed <- function(x, fit1, fit2, constant=FALSE){ # fit1; first layer, fit2; last layer's emulator such as f_M(X2, f1(x2))
   if(constant){
+    c <- 0.001 * 1/attr(scale(fit1$y, center=FALSE), "scaled:scale")
+    
     d <- ncol(fit1$X)
     x <- matrix(x, ncol=d)
     x.mu <- pred.GP(fit1, x)$mu # mean of f1(u)
-    sig2 <- pred.GP(fit1, x)$sig2 # variance of f1(x)
+    sig2 <- c^2*pred.GP(fit1, x)$sig2 # variance of f1(x)
     
     ### calculate the closed form ###
     X2 <- matrix(fit2$X[,-(d+1)], ncol=d)
     w1.x2 <- fit2$X[,d+1]
-    y2 <- fit2$y
+    y2 <- c*fit2$y
     n <- length(y2)
     theta <- fit2$theta
-    tau2hat <- fit2$tau2hat
-    mu2 <- fit2$mu.hat
+    tau2hat <- c^2*fit2$tau2hat
+    mu2 <- c*fit2$mu.hat
     
     Ci <- fit2$Ki
     a <- Ci %*% (y2 - mu2)
@@ -56,12 +58,14 @@ closed <- function(x, fit1, fit2, constant=FALSE){ # fit1; first layer, fit2; la
       predsig2[i] <- pmax(0, tau2hat - (predy[i]-mu2)^2 + sum(mat))
     }
     
-    return(list(mu=predy, sig2=predsig2))
+    return(list(mu=1/c*predy, sig2=1/c^2*predsig2))
   }else{
+    c <- 0.001 * 1/attr(scale(fit1$y, center=FALSE), "scaled:scale")
+    
     d <- ncol(fit1$X)
     x <- matrix(x, ncol=d)
     x.mu <- pred.GP(fit1, x)$mu # mean of f1(u)
-    sig2 <- pred.GP(fit1, x)$sig2 # variance of f1(x)
+    sig2 <- c^2*pred.GP(fit1, x)$sig2 # variance of f1(x)
     
     ### calculate the closed form ###
     X2 <- matrix(fit2$X[,-(d+1)], ncol=d)
@@ -69,10 +73,10 @@ closed <- function(x, fit1, fit2, constant=FALSE){ # fit1; first layer, fit2; la
     y2 <- fit2$y
     n <- length(y2)
     theta <- fit2$theta
-    tau2hat <- fit2$tau2hat
+    tau2hat <- c^2*fit2$tau2hat
     
     Ci <- fit2$Ki
-    a <- Ci %*% (y2 + attr(y2, "scaled:center"))
+    a <- Ci %*% (c*(y2 + attr(y2, "scaled:center")))
     
     ### scale new inputs ###
     x <- t((t(x)-attr(fit2$X,"scaled:center")[1:d])/attr(fit2$X,"scaled:scale")[1:d])
@@ -112,28 +116,30 @@ closed <- function(x, fit1, fit2, constant=FALSE){ # fit1; first layer, fit2; la
       predsig2[i] <- pmax(0, tau2hat - predy[i]^2 + sum(mat))
     }
     
-    return(list(mu=predy, sig2=predsig2))
+    return(list(mu=1/c*predy, sig2=1/c^2*predsig2))
   }
 }
 
 closed2 <- function(x, fit1, fit2, fit3, constant=FALSE){ # fit1; first layer, fit2; second layer f_M(X2, f1(X2)), fit3; last layer f_H(X3, f_M(X3, f1(X3)))
   if(constant){
+    c <- 0.001 * 1/attr(scale(fit2$y, center=FALSE), "scaled:scale")
+    
     d <- ncol(fit1$X)
     x <- matrix(x, ncol=d)
     
     ### second layer's output of test data 
     closed1 <- closed(x, fit1, fit2, constant=TRUE)
     x.mu <- closed1$mu
-    sig2 <- closed1$sig2
+    sig2 <- c^2*closed1$sig2
     
     ### calculate the closed form ###
     X3 <- matrix(fit3$X[,-(d+1)], ncol=d)
     w2.x3 <- fit3$X[,d+1]
-    y3 <- fit3$y
+    y3 <- c*fit3$y
     n <- length(y3)
     theta <- fit3$theta
-    tau2hat <- fit3$tau2hat
-    mu3 <- fit3$mu.hat
+    tau2hat <- c^2*fit3$tau2hat
+    mu3 <- c*fit3$mu.hat
     
     Ci <- fit3$Ki
     a <- Ci %*% (y3 - mu3)
@@ -176,15 +182,17 @@ closed2 <- function(x, fit1, fit2, fit3, constant=FALSE){ # fit1; first layer, f
       predsig2[i] <- pmax(0, tau2hat - (predy[i]-mu3)^2 + sum(mat))
     }
     
-    return(list(mu=predy, sig2=predsig2))
+    return(list(mu=1/c*predy, sig2=1/c^2*predsig2))
   }else{
+    c <- 0.001 * 1/attr(scale(fit2$y, center=FALSE), "scaled:scale")
+    
     d <- ncol(fit1$X)
     x <- matrix(x, ncol=d)
     
     ### second layer's output of test data 
     closed1 <- closed(x, fit1, fit2)
     x.mu <- closed1$mu
-    sig2 <- closed1$sig2
+    sig2 <- c^2*closed1$sig2
     
     ### calculate the closed form ###
     X3 <- matrix(fit3$X[,-(d+1)], ncol=d)
@@ -192,10 +200,10 @@ closed2 <- function(x, fit1, fit2, fit3, constant=FALSE){ # fit1; first layer, f
     y3 <- fit3$y
     n <- length(y3)
     theta <- fit3$theta
-    tau2hat <- fit3$tau2hat
+    tau2hat <- c^2*fit3$tau2hat
     
     Ci <- fit3$Ki
-    a <- Ci %*% (y3 + attr(y3, "scaled:center"))
+    a <- Ci %*% (c*(y3 + attr(y3, "scaled:center")))
     
     ### scale new inputs ###
     x <- t((t(x)-attr(fit3$X,"scaled:center")[1:d])/attr(fit3$X,"scaled:scale")[1:d])
@@ -235,6 +243,6 @@ closed2 <- function(x, fit1, fit2, fit3, constant=FALSE){ # fit1; first layer, f
       predsig2[i] <- pmax(0, tau2hat - predy[i]^2 + sum(mat))
     }
     
-    return(list(mu=predy, sig2=predsig2))
+    return(list(mu=1/c*predy, sig2=1/c^2*predsig2))
   }
 }
